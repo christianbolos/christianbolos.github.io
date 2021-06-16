c96cc78a54dbf04d7ed2f3f64e9ce0e08d41ffd9
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2021-06-15 20:16:02 -0700

    Removed unnecessary css file and styled name on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Banner.css';
-import '../css/PseudoBootstrap.css';
 import {Row, Col, TabContainer, TabContent, TabPane, Nav} from "react-bootstrap";
 
 export default class Banner extends Component {
@@ -11,9 +10,12 @@ export default class Banner extends Component {
                 <div>
                     <Row className="black-bg m-0">
                         <Col md={9}>
-                            <h1 className="text-bold text-white text-start my-4">
+                            <h1 className="text-bold text-white text-start ml-4 mt-4 mb-0">
                                 Christian Bolos
                             </h1>
+                            <h2 className="text-start text-grey ml-4 mt-0 mb-4">
+                                VFS Graduate - Characters/Environments/Concept Art
+                            </h2>
                         </Col>
                         <Col className="align-self-end" md={3}>
                             <Nav fill variant="tabs" className="justify-content-center">
