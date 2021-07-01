e266e9a68d6dce3e02f0fd7be82ae33ef7485bb8
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2021-07-01 14:30:57 -0700

    Implemented scrolling in tall gallery images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,19 +9,20 @@ export default class Gallery extends Component {
             <div>
                 <TabContent>
                     <TabPane eventKey="showcase">
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img class="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a className="gallery-img" data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/><div className="caption-overlay"></div></a>
+                        <a data-fancybox="gallery" href="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png"><img className="center-preview" src="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a className="test" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
                     </TabPane>
                     <TabPane eventKey="characters">
-                        <a class="center-preview" data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
+                        <a className="center-preview" data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
                     </TabPane>
                     <TabPane eventKey="environments">
                         <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
