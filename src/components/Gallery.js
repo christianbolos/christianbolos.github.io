40040f22f45984497b13aa48ef442bcac9d10f6d
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2021-07-01 15:05:32 -0700

    Proof of concept for captions on hover and in-image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,8 +9,30 @@ export default class Gallery extends Component {
             <div>
                 <TabContent>
                     <TabPane eventKey="showcase">
-                        <a className="gallery-img" data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/><div className="caption-overlay"></div></a>
-                        <a data-fancybox="gallery" href="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png"><img className="center-preview" src="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" alt=""/></a>
+                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623" data-caption="Test<br>Test">
+                            <div className="gallery-img">
+                                <img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/>
+                                <div className="caption-overlay">
+                                    <div className="caption-overlay-inner">
+                                        <div className="caption-text">
+                                            This is a very long title test that is very long
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a data-fancybox="gallery" href="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" data-caption="Test">
+                            <div className="gallery-img">
+                                <img className="center-preview" src="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" alt=""/>
+                                <div className="caption-overlay">
+                                    <div className="caption-overlay-inner">
+                                        <div className="caption-text">
+                                            This is a very long title test that is very long
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
                         <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
                         <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
                         <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623"><img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/></a>
