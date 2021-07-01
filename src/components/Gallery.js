06734dbe15a206505000367e7562c6ba9ed08366
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2021-07-01 16:34:13 -0700

    Image iteration progress push

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,25 +2,39 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Gallery.css';
 import {TabContent, TabPane} from "react-bootstrap";
+var images = [];
 
 export default class Gallery extends Component {
+    constructor() {
+        super();
+        images = this.importAll(require.context('../../public/showcase/', false, /\.(png|jpe?g|svg)$/));
+    }
+
+    importAll(r) {
+        return r.keys().map(r);
+    }
+
     render() {
         return (
             <div>
                 <TabContent>
                     <TabPane eventKey="showcase">
-                        <a data-fancybox="gallery" href="https://source.unsplash.com/lSXpV8bDeMA/416x623" data-caption="Test<br>Test">
-                            <div className="gallery-img">
-                                <img className="center-preview" src="https://source.unsplash.com/lSXpV8bDeMA/416x623" alt=""/>
-                                <div className="caption-overlay">
-                                    <div className="caption-overlay-inner">
-                                        <div className="caption-text">
-                                            This is a very long title test that is very long
+                        {
+                            images.map((image, index) =>            
+                                <a data-fancybox="gallery" href={image.default} data-caption="Test<br>Test">
+                                    <div className="gallery-img">
+                                        <img key={index} className="center-preview" src={image.default} alt=""/>
+                                        <div className="caption-overlay">
+                                            <div className="caption-overlay-inner">
+                                                <div className="caption-text">
+                                                    This is a very long title test that is very long
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        </a>
+                                </a>
+                            )
+                        }
                         <a data-fancybox="gallery" href="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" data-caption="Test">
                             <div className="gallery-img">
                                 <img className="center-preview" src="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/9/d9aada40f2e4927b10c4f44c1d5f24618482e959.png" alt=""/>
@@ -54,6 +68,6 @@ export default class Gallery extends Component {
                     </TabPane>
                 </TabContent>
             </div>
-        );
+        )
     }
 }
